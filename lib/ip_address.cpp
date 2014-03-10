24cca4c50e5ee2ea6b4d8fa82ae180dfc3e5fbb8
hylke <weary@weary.nl>
2014-03-10 22:13:13 +0100

    Tiny performance improvement by inlining ip-address comparison

--- lib/ip_address.cpp
+++ lib/ip_address.cpp
@@ -6,25 +6,9 @@
 #include "reass/ip_address.h"
 #include "reass/helpers/misc.h"
 #include <arpa/inet.h>
-#include <boost/functional/hash/hash.hpp>
 #include <netinet/in.h>
 
 BOOST_STATIC_ASSERT_MSG(sizeof(in6_addr) == 128/8, "ipv6 addresses must be 128 bits");
-
-static inline
-bool operator ==(const in6_addr &l, const in6_addr &r)
-{
-#ifdef __SIZEOF_INT128__
-	return  // use gcc's builtin 128-bit type for compare
-		*reinterpret_cast<const __uint128_t *>(&l)
-		==
-		*reinterpret_cast<const __uint128_t *>(&r);
-#else
-	const uint64_t *lu = reinterpret_cast<const uint64_t *>(&l);
-	const uint64_t *ru = reinterpret_cast<const uint64_t *>(&r);
-	return lu[0] == ru[0] && lu[1] == ru[1];
-#endif
-}
 
 BOOST_STATIC_ASSERT_MSG(offsetof(ip_address_t, v4.sin_port) == offsetof(ip_address_t, v6.sin6_port), "structure alignment issue");
 
@@ -53,32 +37,5 @@ std::ostream &operator <<(std::ostream &os, const ip_address_t &ip)
 	else
 		os << '[' << addr << "]:" << ip.port();
 	return os;
-}
-
-// compares ip and port
-bool operator ==(const ip_address_t &l, const ip_address_t &r)
-{
-	if (l.v4.sin_family != r.v4.sin_family)
-		return false;
-	if (l.v4.sin_port != r.v4.sin_port)
-		return false;
-
-	if (l.v4.sin_family == AF_INET)
-		return l.v4.sin_addr.s_addr == r.v4.sin_addr.s_addr;
-	else
-		return l.v6.sin6_addr == r.v6.sin6_addr;
-}
-
-std::size_t hash_value(const ip_address_t &s)
-{
-	std::size_t r = s.v4.sin_family;
-	boost::hash_combine(r, s.v4.sin_port);
-	if (s.v4.sin_family == AF_INET)
-		boost::hash_combine(r, s.v4.sin_addr.s_addr);
-	else
-		for (int n=0; n<4; ++n)
-			boost::hash_combine(r, s.v6.sin6_addr.__in6_u.__u6_addr32[n]);
-
-	return r;
 }
 
